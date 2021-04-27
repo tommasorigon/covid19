3451007fafed9cb0cd25b3e638bbb94d31e116ea
Matteo Pelagatti <matteo.pelagatti@unimib.it>
2021-04-27 17:04:58 +0200

    New version with static models
    I estimated all state space models on data ranging form 2020-09-15 to 2021-04-25 and saved tema as a Rdata file.
    When the app starts it loads data and models and run the smoother using new data and previously estimated models.
    Last-day data can now be seen also in a scatterplot, however I would like to hide the tab of the scatterplot when Nationale is selected. This is a fix to do.

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-# Load packages
 library(shiny)
 library(shinythemes)
 library(KFAS)
@@ -7,11 +6,50 @@ library(dygraphs)
 library(DT)
 library(tidyverse)
 library(gtrendsR)
+library(plotly)
 
 # Carica le funzioni utili
-source("kalman_f.R")
-source("gtrends.R")
-source("download_data.R")
+source("funzioni.R")
+
+# Carica dati di popolazione
+pop_province <- read.csv("province.csv")
+pop_regioni  <- read.csv("regioni.csv")
+
+# Dati vaccini
+raw_vacc_regione <- jsonlite::fromJSON(txt = "https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.json")$data
+data_vacc_reg <- data.frame(
+  data = as.Date(strtrim(raw_vacc_regione$data_somministrazione, 10)),
+  regione = raw_vacc_regione$nome_area,
+  fornitore = raw_vacc_regione$fornitore,
+  prima_dose = raw_vacc_regione$prima_dose,
+  seconda_dose = raw_vacc_regione$seconda_dose,
+  dosi_totali = raw_vacc_regione$prima_dose + raw_vacc_regione$seconda_dose,
+  fascia_anagrafica = raw_vacc_regione$fascia_anagrafica
+)
+
+
+# Carica dati regionali e provinciali
+dt_reg <- load_regione()
+dt_pro <- load_provincia()
+
+# dati di riferimento
+max_date <- max(dt_reg$positivi$data)
+min_date <- min(dt_reg$positivi$data)
+
+# normalizza rispetto a popolazione
+dt_reg_pop <- lapply(dt_reg, divide_by_pop, pop = pop_regioni)
+dt_pro_pop <- divide_by_pop(dt_pro, pop_province)
+
+# Carica modelli state-space
+load("modelli.Rdata") # mod_reg: lista con positivi, decessi, terapie_intensive con liste di modelli
+                      # mod_pro: lista di modelli
+
+# Passa lo smoother sui dati
+smo_reg_pos <- smoother(dt_reg_pop$positivi, mod_reg$positivi)
+smo_reg_dec <- smoother(dt_reg_pop$decessi, mod_reg$decessi)
+smo_reg_icu <- smoother(dt_reg_pop$terapie_intensive, mod_reg$terapie_intensive)
+smo_pro_pos <- smoother(dt_pro_pop, mod_pro)
+
 
 # Define UI --------------------------------------------------------------
 ui <- fluidPage(
@@ -51,7 +89,7 @@ ui <- fluidPage(
             condition = "input.datatype == 'Regionale'",
             selectInput(
               inputId = "region", label = strong("Seleziona regione"),
-              choices = unique(data_reg$regione), multiple = T,
+              choices = names(dt_reg$positivi)[-1], multiple = T,
               selected = c("Lombardia", "Veneto")
             )
           ),
@@ -60,7 +98,7 @@ ui <- fluidPage(
             condition = "input.datatype == 'Provinciale'",
             selectInput(
               inputId = "prov", label = strong("Seleziona provincia"),
-              choices = sort(unique(data_prov$provincia)), multiple = T,
+              choices = sort(names(dt_pro)[-1]), multiple = T,
               selected = c("Milano", "Venezia")
             ),
           ),
@@ -84,11 +122,17 @@ ui <- fluidPage(
               hr()
             ),
             tabPanel(
-              "Ultimo dato disponibile",
+              "Ultimo dato disponibile (tabella)",
               hr(),
               textOutput("tab_testo"),
               hr(),
               DTOutput("tabella"),
+              hr()
+            ),
+            tabPanel(
+              "Ultimo dato disponibile (grafico)",
+              hr(),
+              plotlyOutput("dispersione"),
               hr()
             ),
             tabPanel(
@@ -200,52 +244,6 @@ server <- function(input, output) {
     )
   })
 
-  dati_positivi_regione <- reactive({
-
-    # New positive data
-    data_plot <- aggregate(cbind(contagiati, Pop) ~ data + regione, sum, data = data_reg[data_reg$regione %in% input$region, ])
-    data_plot$contagiati <- pmax(data_plot$contagiati, 0) / data_plot$Pop * 100000
-    data_plot <- data_plot %>% pivot_wider(id_cols = data, names_from = regione, values_from = contagiati)
-    data_plot$Italia <- aggregate(contagiati ~ data, sum, data = data_reg)$contagiati / sum(pop_regioni$Pop) * 100000
-
-    trend <- get_cmp(data_plot)
-    list(
-      livello = xts(trend$livello, data_plot$data),
-      crescita = xts(trend$crescita, data_plot$data)
-    )
-  })
-
-  dati_decessi_regione <- reactive({
-    # First region
-    data_plot <- aggregate(cbind(decessi, Pop) ~ data + regione, sum, data = data_reg[data_reg$regione %in% input$region, ])
-    data_plot$decessi <- diff(c(0, data_plot$decessi))
-    data_plot$decessi <- pmax(data_plot$decessi, 0) / data_plot$Pop * 100000
-    data_plot <- data_plot %>% pivot_wider(id_cols = data, names_from = regione, values_from = decessi)
-    data_plot$Italia <- aggregate(decessi ~ data, sum, data = data_reg)$decessi
-    data_plot$Italia <- pmax(diff(c(0, data_plot$Italia)), 0) / sum(pop_regioni$Pop) * 100000
-
-    trend <- get_cmp(data_plot)
-    list(
-      livello = xts(trend$livello, data_plot$data),
-      crescita = xts(trend$crescita, data_plot$data)
-    )
-  })
-
-  dati_terapia_regione <- reactive({
-
-    # First region
-    data_plot <- aggregate(cbind(terapia_intensiva, Pop) ~ data + regione, sum, data = data_reg[data_reg$regione %in% input$region, ])
-    data_plot$contagiati <- pmax(data_plot$terapia_intensiva, 0) / data_plot$Pop * 100000
-    data_plot <- data_plot %>% pivot_wider(id_cols = data, names_from = regione, values_from = terapia_intensiva)
-    data_plot$Italia <- aggregate(terapia_intensiva ~ data, sum, data = data_reg)$terapia_intensiva / sum(pop_regioni$Pop) * 100000
-
-    trend <- get_cmp(data_plot)
-    list(
-      livello = xts(trend$livello, data_plot$data),
-      crescita = xts(trend$crescita, data_plot$data)
-    )
-  })
-
   dati_dosi_totali_nazionale <- reactive({
     vaccino <- input$vaccine
     if (vaccino != "Tutti") {
@@ -260,8 +258,8 @@ server <- function(input, output) {
 
     trend <- get_cmp(data_plot)
     list(
-      livello = xts(trend$livello, data_plot$data),
-      crescita = xts(trend$crescita, data_plot$data)
+      livello = xts(trend$livello[, -1], trend$livello$data),
+      crescita = xts(trend$crescita[, -1], trend$crescita$data)
     )
   })
 
@@ -280,19 +278,24 @@ server <- function(input, output) {
 
     trend <- get_cmp(data_plot)
     list(
-      livello = xts(trend$livello, data_plot$data),
-      crescita = xts(trend$crescita, data_plot$data)
+      livello = xts(trend$livello[, -1], trend$livello$data),
+      crescita = xts(trend$crescita[, -1], trend$crescita$data)
     )
   })
 
   dati_google_trends_nazionale <- reactive({
-    gtrend_data <- tibble(get_gtrend(keyword = input$keyword, region = "Italia", from = max_date - 180, to = max_date, output = "data.frame")) %>% transmute(date = date, Google = hits)
+    gtrend_data <- get_gtrend(keyword = input$keyword,
+                              region = "Italia",
+                              from = max_date - 180,
+                              to = max_date,
+                              output = "data.frame") %>%
+      transmute(data = date, Google = hits)
     trend <- get_cmp(gtrend_data)
-    trend$livello <- trend$livello / max(trend$livello) * 100
+    trend$livello[, -1] <- trend$livello[, -1] / max(trend$livello[, -1]) * 100
 
     list(
-      livello = xts(trend$livello, gtrend_data$date),
-      crescita = xts(trend$crescita, gtrend_data$date)
+      livello  = xts(trend$livello[, -1],  trend$livello$data),
+      crescita = xts(trend$crescita[, -1], trend$crescita$data)
     )
   })
 
@@ -306,30 +309,40 @@ server <- function(input, output) {
     if (input$datatype != "Provinciale") {
       if (input$type == "Nuovi positivi") {
         type <- input$type
-        data_plot <- dati_positivi_regione()$livello[, region]
+        data_plot <- smo_reg_pos$livello[(smo_reg_pos$livello$data >= input$date[1]) &
+                                         (smo_reg_pos$livello$data <= input$date[2]),
+                                         c("data", region)]
       } else if (input$type == "Decessi") {
         type <- input$type
-        data_plot <- dati_decessi_regione()$livello[, region]
+        data_plot <- smo_reg_dec$livello[(smo_reg_dec$livello$data >= input$date[1]) &
+                                         (smo_reg_dec$livello$data <= input$date[2]),
+                                         c("data", region)]
       } else {
         type <- input$type
-        data_plot <- dati_terapia_regione()$livello[, region]
+        data_plot <- smo_reg_icu$livello[(smo_reg_icu$livello$data >= input$date[1]) &
+                                         (smo_reg_icu$livello$data <= input$date[2]),
+                                         c("data", region)]
       }
     } else {
       type <- "Nuovi positivi"
-      data_plot <- dati_positivi_provincia()$livello[, input$prov]
+      data_plot <- smo_pro_pos$livello[(smo_pro_pos$livello$data >= input$date[1]) &
+                                       (smo_pro_pos$livello$data <= input$date[2]),
+                                       c("data", input$prov)]
     }
 
 
-    data_plot <- data_plot[(index(data_plot) >= input$date[1]) & (index(data_plot) <= input$date[2]), ]
-    dygraph(data_plot, main = paste(type, "ogni 100.000 abitanti"), ylab = paste(type, "ogni 100.000 abitanti")) %>%
-      dyOptions(colors = RColorBrewer::brewer.pal(8, "Dark2"), axisLineWidth = 1.5, fillGraph = TRUE, drawGrid = FALSE)
+    data_plot <- xts(data_plot[, -1, drop = FALSE], data_plot$data)
+    dygraph(data_plot, main = paste(type, "ogni 100.000 abitanti"),
+            ylab = paste(type, "ogni 100.000 abitanti")) %>%
+      dyOptions(colors = RColorBrewer::brewer.pal(8, "Dark2"), axisLineWidth = 1.5,
+                fillGraph = TRUE, drawGrid = FALSE)
   })
 
   output$vaccini <- renderDygraph({
     if (input$datatype2 == "Nazionale") {
       data_plot <- dati_dosi_totali_nazionale()$livello
     } else if (input$datatype2 == "Regionale") {
-      data_plot <- dati_dosi_totali_regione()$livello[, input$region3]
+      data_plot <- dati_dosi_totali_regione()$livello
     }
 
     dygraph(data_plot, main = paste("Dosi totali giornaliere somministrate")) %>%
@@ -342,24 +355,32 @@ server <- function(input, output) {
     } else if (input$datatype == "Regionale") {
       region <- input$region
     }
-
+    
     if (input$datatype != "Provinciale") {
       if (input$type == "Nuovi positivi") {
         type <- input$type
-        data_plot <- dati_positivi_regione()$crescita[, region]
+        data_plot <- smo_reg_pos$crescita[(smo_reg_pos$crescita$data >= input$date[1]) &
+                                          (smo_reg_pos$crescita$data <= input$date[2]),
+                                         c("data", region)]
       } else if (input$type == "Decessi") {
         type <- input$type
-        data_plot <- dati_decessi_regione()$crescita[, region]
+        data_plot <- smo_reg_dec$crescita[(smo_reg_dec$crescita$data >= input$date[1]) &
+                                          (smo_reg_dec$crescita$data <= input$date[2]),
+                                         c("data", region)]
       } else {
         type <- input$type
-        data_plot <- dati_terapia_regione()$crescita[, region]
+        data_plot <- smo_reg_icu$crescita[(smo_reg_icu$crescita$data >= input$date[1]) &
+                                          (smo_reg_icu$crescita$data <= input$date[2]),
+                                         c("data", region)]
       }
     } else {
       type <- "Nuovi positivi"
-      data_plot <- dati_positivi_provincia()$crescita[, input$prov]
+      data_plot <- smo_pro_pos$crescita[(smo_pro_pos$crescita$data >= input$date[1]) &
+                                        (smo_pro_pos$crescita$data <= input$date[2]),
+                                       c("data", input$prov)]
     }
-
-    data_plot <- data_plot[(index(data_plot) >= input$date[1]) & (index(data_plot) <= input$date[2])]
+    
+    data_plot <- xts(data_plot[, -1], data_plot$data)
     dygraph(data_plot,
       main = "Tasso di crescita giornaliero (%)", ylab = "Tasso di crescita giornaliero (%)"
     ) %>%
@@ -368,21 +389,23 @@ server <- function(input, output) {
   })
 
   output$gtrends <- renderDygraph({
-    data_plot <- dati_positivi_regione()$livello[, "Italia"]
-    data_plot <- data_plot[(index(data_plot) >= max_date - 180) & (index(data_plot) <= max_date), ]
+    data_plot <- smo_reg_pos$livello[smo_reg_pos$livello$data >= max_date - 180,
+                                     c("data", "Italia")]
     data_plot$Italia <- data_plot$Italia / max(data_plot$Italia) * 100
+    data_plot <- xts(data_plot$Italia, data_plot$data)
     gtrend <- dati_google_trends_nazionale()$livello
-    data_plot <- merge(gtrend, data_plot, all = TRUE)
+    data_plot <- cbind(Google = gtrend, Positivi = data_plot)
 
     dygraph(data_plot, main = paste("Nuovi positivi vs Google trend"), ylab = "Scala arbitraria (min 0, max 100)") %>%
       dyOptions(colors = RColorBrewer::brewer.pal(8, "Dark2"), axisLineWidth = 1.5, fillGraph = TRUE, drawGrid = FALSE)
   })
 
   output$tassi_gtrends <- renderDygraph({
-    data_plot <- dati_positivi_regione()$crescita[, "Italia"]
-    data_plot <- data_plot[(index(data_plot) >= max_date - 180) & (index(data_plot) <= max_date), ]
+    data_plot <- smo_reg_pos$crescita[smo_reg_pos$crescita$data >= max_date - 180,
+                                     c("data", "Italia")]
     gtrend <- dati_google_trends_nazionale()$crescita
-    data_plot <- merge(gtrend, data_plot, all = TRUE)
+    data_plot <- xts(data_plot$Italia, data_plot$data)
+    data_plot <- cbind(Google = gtrend, Positivi = data_plot)
 
     dygraph(data_plot, main = paste("Tasso di crescita (%) - Nuovi positivi vs Google trend"), ylab = "Tasso di crescita (%)") %>%
       dyLimit(limit = 0, strokePattern = "dashed") %>%
@@ -397,10 +420,10 @@ server <- function(input, output) {
     if (input$datatype == "Nazionale") {
       dt <- tibble(
         Regione = "Italia",
-        `Casi per 100k abitanti` = as.numeric(tail(dati_positivi_regione()$livello[, "Italia"], 1)),
-        `Crescita casi (%)` = as.numeric(tail(dati_positivi_regione()$crescita[, "Italia"], 1)),
-        `Decessi per 100k abitanti` = as.numeric(tail(dati_decessi_regione()$livello[, "Italia"], 1)),
-        `Crescita decessi (%)` = as.numeric(tail(dati_decessi_regione()$crescita[, "Italia"], 1)),
+        `Casi per 100k abitanti` = as.numeric(tail(smo_reg_pos$livello[, "Italia"], 1)),
+        `Crescita casi (%)` = as.numeric(tail(smo_reg_pos$crescita[, "Italia"], 1)),
+        `Decessi per 100k abitanti` = as.numeric(tail(smo_reg_dec$livello[, "Italia"], 1)),
+        `Crescita decessi (%)` = as.numeric(tail(smo_reg_dec$crescita[, "Italia"], 1)),
       ) %>%
         datatable(rownames = FALSE, options = list(pageLength = 22, dom = "t")) %>%
         formatStyle(
@@ -412,11 +435,11 @@ server <- function(input, output) {
 
     if (input$datatype == "Regionale") {
       dt <- tibble(
-        Regione = colnames(dati_positivi_regione()$livello),
-        `Casi per 100k abitanti` = as.numeric(tail(dati_positivi_regione()$livello, 1)),
-        `Crescita casi (%)` = as.numeric(tail(dati_positivi_regione()$crescita, 1)),
-        `Decessi per 100k abitanti` = as.numeric(tail(dati_decessi_regione()$livello, 1)),
-        `Crescita decessi (%)` = as.numeric(tail(dati_decessi_regione()$crescita, 1)),
+        Regione = colnames(smo_reg_pos$livello)[-1],
+        `Casi per 100k abitanti` = as.numeric(tail(smo_reg_pos$livello[, -1], 1)),
+        `Crescita casi (%)` = as.numeric(tail(smo_reg_pos$crescita[, -1], 1)),
+        `Decessi per 100k abitanti` = as.numeric(tail(smo_reg_dec$livello[, -1], 1)),
+        `Crescita decessi (%)` = as.numeric(tail(smo_reg_dec$crescita[, -1], 1)),
       ) %>%
         datatable(rownames = FALSE, options = list(pageLength = 22, dom = "t")) %>%
         formatStyle(
@@ -428,11 +451,11 @@ server <- function(input, output) {
 
     if (input$datatype == "Provinciale") {
       dt <- tibble(
-        Provincia = colnames(dati_positivi_provincia()$livello),
-        `Casi per 100k abitanti` = as.numeric(tail(dati_positivi_provincia()$livello, 1)),
-        `Crescita casi (%)` = as.numeric(tail(dati_positivi_provincia()$crescita, 1)),
+        Provincia = colnames(smo_pro_pos$livello)[-1],
+        `Casi per 100k abitanti` = as.numeric(tail(smo_pro_pos$livello[, -1], 1)),
+        `Crescita casi (%)` = as.numeric(tail(smo_pro_pos$crescita[, -1], 1)),
       ) %>%
-        datatable(rownames = FALSE, options = list(pageLength = 22, dom = "t")) %>%
+        datatable(rownames = FALSE, options = list(pageLength = 110, dom = "t")) %>%
         formatStyle(
           columns = c(3, 3),
           color = styleInterval(0, c("black", "red"))
@@ -443,6 +466,54 @@ server <- function(input, output) {
     dt
   })
 
+  output$dispersione <- renderPlotly({
+    if (input$datatype == "Nazionale") return(NULL)
+    if (input$datatype == "Regionale") {
+      dt <- tibble(
+        Regione = colnames(smo_reg_pos$livello)[-1],
+        `Casi per 100k abitanti` = as.numeric(tail(smo_reg_pos$livello[, -1], 1)),
+        `Crescita casi (%)` = as.numeric(tail(smo_reg_pos$crescita[, -1], 1)),
+        `Decessi per 100k abitanti` = as.numeric(tail(smo_reg_dec$livello[, -1], 1)),
+        `Crescita decessi (%)` = as.numeric(tail(smo_reg_dec$crescita[, -1], 1)),
+        `Terapie intensive per 100k abitanti` = as.numeric(tail(smo_reg_icu$livello[, -1], 1)),
+        `Crescita terapie intensive (%)` = as.numeric(tail(smo_reg_icu$crescita[, -1], 1)),
+      ) %>% mutate_at(2:7, function(x) round(x, 2))
+      dt$Selezione <- dt$Regione %in% input$region
+      if (input$type == "Nuovi positivi") {
+        plt <- ggplot(dt, aes(y = `Casi per 100k abitanti`,
+                              x = `Crescita casi (%)`,
+                              label = Regione,
+                              color = Selezione)) + geom_text() + theme(legend.position = "none")
+      } else if (input$type == "Decessi") {
+        plt <- ggplot(dt, aes(y = `Decessi per 100k abitanti`,
+                              x = `Crescita decessi (%)`,
+                              label = Regione,
+                              color = Selezione)) + geom_text() + theme(legend.position = "none")
+      } else {
+        plt <- ggplot(dt, aes(y = `Terapie intensive per 100k abitanti`,
+                              x = `Crescita terapie intensive (%)`,
+                              label = Regione,
+                              color = Selezione)) + geom_text() + theme(legend.position = "none")
+      }
+    }
+    
+    if (input$datatype == "Provinciale") {
+      dt <- tibble(
+        Provincia = colnames(smo_pro_pos$livello)[-1],
+        `Casi per 100k abitanti` = as.numeric(tail(smo_pro_pos$livello[, -1], 1)),
+        `Crescita casi (%)` = as.numeric(tail(smo_pro_pos$crescita[, -1], 1)),
+      ) %>% mutate_at(2:3, function(x) round(x, 1))
+      dt$Selezione <- dt$Provincia %in% input$prov
+      plt <- ggplot(dt, aes(y = `Casi per 100k abitanti`,
+                   x = `Crescita casi (%)`,
+                   label = Provincia,
+                   color = Selezione)) +
+        geom_text()
+    }
+    ggplotly(plt, dynamicTicks = TRUE)
+  })
+  
+  
   output$tbl <- renderDT({
     regione <- input$region3
 
